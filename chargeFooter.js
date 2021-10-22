528a4a78ec6eba0d988474cf25855614d5e1d510
iFaaka <alvarezfacundo321@gmail.com>
2021-10-22 00:28:19 -0300

    Pagina de contacto
    v1 de Pagina de contacto, form y iframe

---
+++ chargeFooter.js
@@ -0,0 +1,8 @@
+document.getElementById("footerId").innerHTML =(` 
+
+<p class="footerText">Todos los derechos reservados</p>
+<p class="footerText">Creado por <a href="https://www.linkedin.com/in/facundon-alvarez/" id="linkFooter">Facundo Nicolas Alvarez</a> </p>
+
+
+
+`)
---
+++ chargeNav.js
@@ -0,0 +1,25 @@
+document.getElementById("headerId").innerHTML =(`
+
+<nav id="navId" style="top:0;bottom:0;">   
+            
+<a href="#"><img src="https://i.ibb.co/fSBwCs9/SS.png" id="navImg"></a>
+
+
+
+
+<div id="containerNavLink">
+<a href="index.html" class="navLink">Inicio</a>
+
+<a href="contacto.html" class="navLink">Nuestros Vehiculos</a>
+
+<a href="#" class="navLink">Contacto</a>
+</div>
+
+</nav>
+
+
+`)
+
+
+
+
