ee9128a18e25399e8182f82310a7c24372a997af
iFaaka <alvarezfacundo321@gmail.com>
2021-10-22 00:59:47 -0300

    Contacto + Nav/Footer Dinamicos
    Pagina de contacto lista + Responsive
    Nav y Footer dinamico
    iFrame listo

--- chargeNav.js
+++ chargeNav.js
@@ -2,7 +2,7 @@ document.getElementById("headerId").innerHTML =(`
 
 <nav id="navId" style="top:0;bottom:0;">   
             
-<a href="#"><img src="https://i.ibb.co/fSBwCs9/SS.png" id="navImg"></a>
+<a href="index.html"><img src="https://i.ibb.co/fSBwCs9/SS.png" id="navImg"></a>
 
 
 
@@ -10,9 +10,9 @@ document.getElementById("headerId").innerHTML =(`
 <div id="containerNavLink">
 <a href="index.html" class="navLink">Inicio</a>
 
-<a href="contacto.html" class="navLink">Nuestros Vehiculos</a>
+<a href="" class="navLink">Nuestros Vehiculos</a>
 
-<a href="#" class="navLink">Contacto</a>
+<a href="contacto.html" class="navLink">Contacto</a>
 </div>
 
 </nav>
